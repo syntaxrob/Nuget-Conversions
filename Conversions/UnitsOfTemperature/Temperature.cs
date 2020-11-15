4c9602c2c43624a61f1b0f2499a115b05acf8af0
Rob Penn <rob.penn@genovo.co.uk>
2020-11-15 11:45:02 +0000

    Add in Temerature conversion unit

---
+++ Conversions/UnitsOfTemperature/Temperature.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conversions.UnitsOfTemperature
+{
+    public class Temperature
+    {
+        /// <summary>
+        /// Convert Celcius to Farenheit
+        /// </summary>
+        /// <param name="celius"></param>
+        /// <returns>Farenheit</returns>
+        public static double CelciusToFarenheit(double celius)
+        {
+            return (celius * 1.8) + 32;
+        }
+
+        /// <summary>
+        /// Convert Celcius to Kelvin
+        /// </summary>
+        /// <param name="celcius"></param>
+        /// <returns>Kelvin</returns>
+        public static double CelciusToKelvin(double celcius)
+        {
+            return celcius + 273.15;
+        }
+
+        /// <summary>
+        /// Convert Farenheit to Celcius
+        /// </summary>
+        /// <param name="farenheit"></param>
+        /// <returns>Celcius</returns>
+        public static double FarenheitToCelcius(double farenheit)
+        {
+            return (farenheit - 32) * 0.55555555555555555555555555555556;
+        }
+
+        /// <summary>
+        /// Convert Farenheit to Kelvin
+        /// </summary>
+        /// <param name="farenheit"></param>
+        /// <returns>Kelvin</returns>
+        public static double FarenheitToKelvin(double farenheit)
+        {
+            return ((farenheit - 32) * 0.55555555555555555555555555555556) + 273.15;
+        }
+
+        /// <summary>
+        /// Convert Kelvin to Celcius
+        /// </summary>
+        /// <param name="kelvin"></param>
+        /// <returns>Celcius</returns>
+        public static double KelvinToCelcius(double kelvin)
+        {
+            return kelvin - 273.15;
+        }
+
+        /// <summary>
+        /// Convert Kelvin to Farenheit
+        /// </summary>
+        /// <param name="kelvin"></param>
+        /// <returns>Farenheit</returns>
+        public static double KelvinToFarenheit(double kelvin)
+        {
+            return ((kelvin - 273.15) * 1.8) + 32;
+        }
+    }
+}
---
+++ UnitTests/TemperatureTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Conversions.UnitsOfTemperature;
+using Xunit;
+
+namespace UnitTests
+{
+    public class TemperatureTests
+    {
+        [Fact]
+        public void CelciusToFarenheit()
+        {
+            double expected = 33.8;
+            double actual = Temperature.CelciusToFarenheit(1);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CelciusToKelvin()
+        {
+            double expected = 274.15;
+            double actual = Temperature.CelciusToKelvin(1);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FarenheitToCelcius()
+        {
+            double expected = -17.22222222222222;
+            double actual = Temperature.FarenheitToCelcius(1);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FarenheitToKelvin()
+        {
+            double expected = 255.92777777777775;
+            double actual = Temperature.FarenheitToKelvin(1);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void KelvinToCelcius()
+        {
+            double expected = -272.15;
+            double actual = Temperature.KelvinToCelcius(1);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void KelvinToFarenheit()
+        {
+            double expected = -457.86999999999995;
+            double actual = Temperature.KelvinToFarenheit(1);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
