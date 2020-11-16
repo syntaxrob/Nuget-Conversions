3c2da96286e0374f8b9fda71539426c78aedb5eb
Rob Penn <rob.penn@genovo.co.uk>
2020-11-16 22:51:41 +0000

    Added MPGs

---
+++ Conversions/UnitsOfFuelEconomy/FuelEconomy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conversions.UnitsOfFuelEconomy
+{
+    public class FuelEconomy
+    {
+        private static class ConstValues
+        {
+            public const double MpgMpgImp = 1.201;
+            public const double MpgKpl = 2.352;
+            public const double MpgLhkm = 235.215;
+            public const double MpgImpKpl = 2.825;
+        }
+
+        /// <summary>
+        /// Convert Miles per gallon to Imperial Miles per gallon
+        /// </summary>
+        /// <param name="mpg"></param>
+        /// <returns>Imperial Miles per gallon</returns>
+        public static double MilesPerGallonToMilesPerGallonImperial(double mpg)
+        {
+            return mpg * ConstValues.MpgMpgImp;
+        }
+
+        /// <summary>
+        /// Convert Miles per gallon to Kilometers per liter
+        /// </summary>
+        /// <param name="mpg"></param>
+        /// <returns>Kilometers per liter</returns>
+        public static double MilesPerGallonToKilometersPerLitre(double mpg)
+        {
+            return mpg / ConstValues.MpgKpl;
+        }
+
+        /// <summary>
+        /// Converts Miles per gallon to Liters per 100 km
+        /// </summary>
+        /// <param name="mpg"></param>
+        /// <returns></returns>
+        public static double MilesPerGallonToLitresPerHundredkm(double mpg)
+        {
+            return ConstValues.MpgLhkm / mpg;
+        }
+    }
+}
---
+++ UnitTests/FuelEconomyTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Conversions.UnitsOfFuelEconomy;
+using Xunit;
+
+namespace UnitTests
+{
+    public class FuelEconomyTests
+    {
+        [Fact]
+        public void MilesPerGallonAndMilesPerGallonImperial()
+        {
+            double expected = 1.201;
+            double actual = FuelEconomy.MilesPerGallonToMilesPerGallonImperial(1);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MilesPerGallonAndKilometersPerLitre()
+        {
+            double expected = 0.4251700680272109;
+            double actual = FuelEconomy.MilesPerGallonToKilometersPerLitre(1);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MilesPerGallonAndLitresPerHundredkm()
+        {
+            double expected = 235.215;
+            double actual = FuelEconomy.MilesPerGallonToLitresPerHundredkm(1);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
