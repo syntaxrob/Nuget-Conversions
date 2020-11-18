04193fb584998144b7900e1b7d3dd076fe5dd1c1
Rob Penn <rob.penn@genovo.co.uk>
2020-11-18 13:23:16 +0000

    Added frequency units

---
+++ Conversions/UnitOfFrequency/Frequency.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace Conversions.UnitOfFrequency
+{
+    public class Frequency
+    {
+        private static class ConstValues
+        {
+            public const double HtzKhtz = 1000;
+            public const double HtzMhtz = 1000000;
+            public const double HtzGhtz = 1000000000;
+            public const double KhtzMhtz = 1000;
+            public const double KhtzGhtz = 1000000;
+            public const double MhtzGhtz = 1000;
+        }
+
+        /// <summary>
+        /// Convert Hertz to Kilohertz
+        /// </summary>
+        /// <param name="htz"></param>
+        /// <returns>Kilohertz</returns>
+        public static double HertzToKilohertz(double htz)
+        {
+            return htz / ConstValues.HtzKhtz;
+        }
+
+        /// <summary>
+        /// Convert Hertz to Megahertz
+        /// </summary>
+        /// <param name="htz"></param>
+        /// <returns>Megahertz</returns>
+        public static double HertzToMegahertz(double htz)
+        {
+            return htz / ConstValues.HtzMhtz;
+        }
+
+        /// <summary>
+        /// Convert Hertz to Gigahertz
+        /// </summary>
+        /// <param name="htz"></param>
+        /// <returns>Gigahertz</returns>
+        public static double HertzToGigahertz(double htz)
+        {
+            return htz / ConstValues.HtzGhtz;
+        }
+
+        /// <summary>
+        /// Convert Kilohertz to Hertz
+        /// </summary>
+        /// <param name="khtz"></param>
+        /// <returns>Hertz</returns>
+        public static double KilohertzToHertz(double khtz)
+        {
+            return khtz * ConstValues.HtzKhtz;
+        }
+
+        /// <summary>
+        /// Convert Kilohertz to Megahertz
+        /// </summary>
+        /// <param name="khtz"></param>
+        /// <returns>Megahertz</returns>
+        public static double KilohertzToMegahertz(double khtz)
+        {
+            return khtz / ConstValues.KhtzMhtz;
+        }
+
+        /// <summary>
+        /// Convert Kilohertz to Gigahertz
+        /// </summary>
+        /// <param name="khtz"></param>
+        /// <returns>Gigahertz</returns>
+        public static double KilohertzToGigahertz(double khtz)
+        {
+            return khtz / ConstValues.KhtzGhtz;
+        }
+
+        /// <summary>
+        /// Convert Megahertz to Hertz
+        /// </summary>
+        /// <param name="mhtz"></param>
+        /// <returns>Hertz</returns>
+        public static double MegahertzToHertz(double mhtz)
+        {
+            return mhtz * ConstValues.HtzMhtz;
+        }
+
+        /// <summary>
+        /// Convert Megahertz to Kilohertz
+        /// </summary>
+        /// <param name="mhtz"></param>
+        /// <returns>Kilohertz</returns>
+        public static double MegahertzToKilohertz(double mhtz)
+        {
+            return mhtz * ConstValues.HtzKhtz;
+        }
+
+        /// <summary>
+        /// Convert Megahertz to Gigahertz
+        /// </summary>
+        /// <param name="mhtz"></param>
+        /// <returns>Gigahertz</returns>
+        public static double MegahertzToGigahertz(double mhtz)
+        {
+            return mhtz / ConstValues.MhtzGhtz;
+        }
+
+        /// <summary>
+        /// Convert Gigahertz to Hertz
+        /// </summary>
+        /// <param name="ghtz"></param>
+        /// <returns>Hertz</returns>
+        public static double GigahertzToHertz(double ghtz)
+        {
+            return ghtz * ConstValues.HtzGhtz;
+        }
+
+        /// <summary>
+        /// Convert Gigahertz to Kilohertz
+        /// </summary>
+        /// <param name="ghtz"></param>
+        /// <returns>Kilohertz</returns>
+        public static double GigahertzToKilohertz(double ghtz)
+        {
+            return ghtz * ConstValues.KhtzGhtz;
+        }
+
+        /// <summary>
+        /// Convert Gigahertz to Megahertz
+        /// </summary>
+        /// <param name="ghtz"></param>
+        /// <returns>Megahertz</returns>
+        public static double GigahertzToMegahertz(double ghtz)
+        {
+            return ghtz * ConstValues.MhtzGhtz;
+        }
+    }
+}
---
+++ UnitTests/FrequencyTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Conversions.UnitOfFrequency;
+using Xunit;
+
+namespace UnitTests
+{
+    public class FrequencyTests
+    {
+        [Fact]
+        public void HertzAndKilohertz()
+        {
+            double expected = 0.001;
+            double actual = Frequency.HertzToKilohertz(1);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void HertzAndMegahertz()
+        {
+            double expected = 0.000001;
+            double actual = Frequency.HertzToMegahertz(1);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void HertzAndGigahertz()
+        {
+            double expected = 0.000000001;
+            double actual = Frequency.HertzToGigahertz(1);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void KilohertzAndMegahertz()
+        {
+            double expected = 0.001;
+            double actual = Frequency.KilohertzToMegahertz(1);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void KilohertzAndGigahertz()
+        {
+            double expected = 0.000001;
+            double actual = Frequency.KilohertzToGigahertz(1);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MegahertzAndGigahertz()
+        {
+            double expected = 0.001;
+            double actual = Frequency.MegahertzToGigahertz(1);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
