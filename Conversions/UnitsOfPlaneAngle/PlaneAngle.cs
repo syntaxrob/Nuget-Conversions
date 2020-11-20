bddad97db3afc957307214303e691631eb7eb306
Rob Penn <rob.penn@genovo.co.uk>
2020-11-20 00:27:36 +0000

    Added Degrees conversions

---
+++ Conversions/UnitsOfPlaneAngle/PlaneAngle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conversions.UnitsOfPlaneAngle
+{
+    public class PlaneAngle
+    {
+        private static class ConstValues
+        {
+            public const double DegGrad = 1.11111111111111;
+            public const double Pi = 3.14159265358979323846;
+            public const double DegMoa = 60;
+            public const double DegSoa = 3600;
+        }
+
+        /// <summary>
+        /// Convert Degrees to Gradians
+        /// </summary>
+        /// <param name="deg"></param>
+        /// <returns>Gradians</returns>
+        public static double DegreeToGradian(double deg)
+        {
+            return deg * ConstValues.DegGrad;
+        }
+
+        /// <summary>
+        /// Convert Degrees to Millirads
+        /// </summary>
+        /// <param name="deg"></param>
+        /// <returns>Millirads</returns>
+        public static double DegreeToMilliradian(double deg)
+        {
+            return deg * ((1000 * ConstValues.Pi) / 180);
+        }
+
+        /// <summary>
+        /// Convert Degrees to Minute of Arc
+        /// </summary>
+        /// <param name="deg"></param>
+        /// <returns>Minute of Arc</returns>
+        public static double DegreeToMinuteOfArc(double deg)
+        {
+            return deg * ConstValues.DegMoa;
+        }
+
+        /// <summary>
+        /// Convert Degrees to Radians
+        /// </summary>
+        /// <param name="deg"></param>
+        /// <returns>Radians</returns>
+        public static double DegreeToRadian(double deg)
+        {
+            return deg * (ConstValues.Pi / 180);
+        }
+
+        /// <summary>
+        /// Convert Degrees to Second of Arc
+        /// </summary>
+        /// <param name="deg"></param>
+        /// <returns>Second of Arc</returns>
+        public static double DegreeToSecondOfArc(double deg)
+        {
+            return deg * ConstValues.DegSoa;
+        }
+    }
+}
---
+++ UnitTests/PlaneAngleTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Conversions.UnitsOfPlaneAngle;
+using Xunit;
+
+namespace UnitTests
+{
+    public class PlaneAngleTests
+    {
+        [Fact]
+        public void DegreesAndGradians()
+        {
+            double expected = 1.11111111111111;
+            double actual = PlaneAngle.DegreeToGradian(1);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void DegreesAndMilliRadians()
+        {
+            double expected = 17.453292519943293;
+            double actual = PlaneAngle.DegreeToMilliradian(1);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void DegreesAndMinutesOfArc()
+        {
+            double expected = 60;
+            double actual = PlaneAngle.DegreeToMinuteOfArc(1);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void DegreesAndRadians()
+        {
+            double expected = 0.017453292519943295;
+            double actual = PlaneAngle.DegreeToRadian(1);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void DegreesAndSecondsOfArc()
+        {
+            double expected = 3600;
+            double actual = PlaneAngle.DegreeToSecondOfArc(1);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
